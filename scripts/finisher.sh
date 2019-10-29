3e508b7bd10e24c73f9f9150f45a3b336bcd3c5d
Martin Smith <m.smith@garvan.org.au>
2019-10-29 12:29:54 +1100

    latest basecalling scripts

--- scripts/finisher.sh
+++ scripts/finisher.sh
@@ -4,12 +4,11 @@
 #$ -S /bin/bash
 #$ -b y
 
-#NUM_JOBS=$(find . -type f -name "*.fast5" | wc -l)
-NUM_ERROR=$(for f in guppy-3.1.5/*/*/*.txt; do nice grep "ERROR" $f; done | wc -l)
-NUM_SUCC=$(for f in guppy-3.1.5/*/*/*.txt; do nice grep "succ" $f; done | wc -l)
- 
-if [ $NUM_ERROR -gt 0 ]; then echo "CUDA ERROR: At least this many jobs failed: "$NUM_ERROR; exit 1; fi
-#if [ ! $NUM_SUCC -eq $NUM_JOBS ]; then echo "MISSING JOBS: "$NUM_SUCC" successful jobs, but total is "$NUM_JOBS; exit 1; fi
+FASTQDIR=./fastq/${1##*/}_guppy-3.2.4
 
-#cat guppy-3.1.5/*/*/seq*.txt > raw.txt && awk ' /^filename/ && FNR > 1 {next} {print $0} ' raw.txt > sequencing_summary.txt && rm raw.txt
-#cat guppy-3.1.5/*/*/*.fastq > HAC.fastq && gzip HAC.fastq
+find ${FASTQDIR} -name "*fastq" -exec cat {} \; > ${FASTQDIR}.fastq
+
+( head -n 1 ${FASTQDIR}/1/sequencing_summary.txt ; find ${FASTQDIR} -name "*_summary.txt" -exec cat {} \; | grep -v filename ) > ./seq_sums/${1##*/}_guppy-3.2.4_seqsum.txt
+
+rm -rf ${FASTQDIR} 
+
--- scripts/launcher_guppy_files.sh
+++ scripts/launcher_guppy_files.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-NUM_JOBS=$(find . -type f -name "*.fast5" | wc -l)
+INPUT=${1%*/} #chop off last slash
 
-qsub -t 1-${NUM_JOBS} ./guppy_basecall_files.sge
+NUM_JOBS=$(find ${1} -type f -name "*.fast5" | wc -l)
 
-#AJOBID=$( $QCMD ) # keep job id
+CMD="qsub -sync y -t 1-${NUM_JOBS} ./scripts/guppy_basecall_files.sge ${INPUT}"
 
-#qsub -hold_jid $AJOBID "./finisher.sh"
+echo $CMD 
+
+WAIT4IT=$( $CMD )
+
+qsub -hold_jid $WAIT4IT ./scripts/finisher.sh ${INPUT}

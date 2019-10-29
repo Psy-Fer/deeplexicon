01a990c50d5026ac357c1e7e75531945ccfc0542
Martin Smith <m.smith@garvan.org.au>
2019-10-29 13:02:46 +1100

    get uniquely mapping reads

--- scripts/minimap2.sh
+++ scripts/minimap2.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-
+#$ -cwd
+#$ -N MM2
+#$ -S /bin/bash
+#$ -b y
+#$ -j y
+#$ -l mem_requested=2G
+#$ -l h_vmem=2G
+#$ -l tmp_requested=50M
+#$ -pe smp 12
+#$ -o logs/mapping.sge.log
 module load marsmi/nanopore/minimap2
 
 FQ=$2
 FA=$1
-ALNPAF=${FQ%*.fastq.gz}.paf
+ALNPAF=${FQ%*.fastq}.paf
 OUTPUT=./aln/${ALNPAF##*/}
 THREADS=12
 
@@ -19,11 +28,17 @@ if [[ ! -d aln ]] ; then
   mkdir aln
 fi
 
-echo "running minimap2..."
+echo "running minimap2, version :"
+$MM2_BIN -v
+
 time $MM2_BIN --secondary=no -k14 -t ${THREADS} ${FA} ${FQ} > ${OUTPUT}
 
-echo "Copying data..."
+# Get readIDs for uniquely mapping reads
+UNIQ_ID=${OUTPUT%*.paf}_MQ60_uniqIDs.txt
+awk '$12 == 60' ${OUTPUT}  | cut -f 1,6 | sort -k 1,1 | cut -f 1 | uniq -c | sort -rn | awk '{ if ($1 < 2 && $1 > 0) print $2}' > ${UNIQ_ID}
 
+# Filter reads for high-quality unique mappers
+join <( sort -k 1,1 ${OUTPUT} | awk '$12 == 60' ) <( sort ${UNIQ_ID} ) | sed -e 's/ /\t/g' > ${OUTPUT%*.paf}_MQ60_uniq.paf
 
 echo "Done!"
 
